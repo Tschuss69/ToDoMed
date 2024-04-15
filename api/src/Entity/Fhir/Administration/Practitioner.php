e93923aeefbdaa4dfcca28f4fba2161bd9386c32
Tschuss69 <mathieu.schaaf@gmail.com>
2024-04-15 10:42:05 +0200

    correction practitionner et ajout gitignore

--- api/src/Entity/Fhir/Administration/Practitioner.php
+++ api/src/Entity/Fhir/Administration/Practitioner.php
@@ -3,8 +3,11 @@
 namespace App\Entity\Fhir\Administration;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\State\Processor\UserProcessor;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -20,6 +23,11 @@ use App\Entity\Fhir\Foundation\DataTypes\HumanName as HumanName;
     mercure: true
 )]
 #[ORM\Entity]
+#[Get]
+#[Post(processor: UserProcessor::class)]
+#[Delete]
+#[Put]
+#[GetCollection]
 class Practitioner extends User
 {
     #[ORM\OneToMany(mappedBy: 'practitioner', targetEntity: HumanName::class, cascade: ['persist', 'remove'], orphanRemoval: true)]
